efcd4f29803496b2ae9c9bd3a751015c8d47e869
Fulva1230 <b10503137@gapps.ntust.edu.tw>
2021-12-31 14:23:00 +0800

    new kotlin version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 import kr.entree.spigradle.kotlin.*
 
 plugins {
-    kotlin("jvm") version "1.6.0"
-    kotlin("kapt") version "1.6.0"
+    kotlin("jvm") version "1.6.10"
+    kotlin("kapt") version "1.6.10"
     id("kr.entree.spigradle") version "2.2.4"
     id("com.github.johnrengelman.shadow") version "7.1.0"
 }
